f9aea772359e44f689b36c9ac0629b20d9ef104c
Michael Eskarous <mneskarous@gmail.com>
2019-11-22 15:32:25 -0800

    Updates

---
+++ precache-manifest.b596bd10b636aedd73ffacdb5684c000.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc7d2f87d53683440a79c44c24cb6dc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "948dc8e66d5c4f095144",
+    "url": "/monsters-rolodex/static/css/main.e909f96b.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "948dc8e66d5c4f095144",
+    "url": "/monsters-rolodex/static/js/main.93ce3d3a.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
